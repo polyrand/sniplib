2e0ca61475fea786077d2b113ee3859e4b3ca476
rickaa <37962604+d0l-phin@users.noreply.github.com>
2019-08-21 22:50:29 +0200

    Enable more configuration
    Updated behaviour, now the snippets folder has to be the first argument
    and the query the following ones. This way you can use whatever folder
    you want.

--- sniplib.sh
+++ sniplib.sh
@@ -23,18 +23,18 @@ sl() {
     # https://stackoverflow.com/questions/604864/print-a-file-skipping-first-x-lines-in-bash
     local N=1
 
-    out=("$(rg "(\"{3}|'{3})[\sa-z0-9]*(\"{3}|'{3})" snips | fzf --delimiter=:\
+    out=("$(rg "(\"{3}|'{3})[\sa-z0-9]*(\"{3}|'{3})" $1 | fzf --delimiter=:\
     --preview='[[ $(file --mime {1}) =~ binary ]]\
     && echo {1} is a binary file || (bat --style=numbers\
     --color=always {1} || highlight -O ansi -l {1} || coderay {1} ||\
-    rougify {1} || cat {1}) 2> /dev/null | head -500' --query="$1"\
+    rougify {1} || cat {1}) 2> /dev/null | head -500' --query="${@:2}"\
     --exit-0 --expect=ctrl-e)")
 
     key=$(head -1 <<< "$out")
 
     file=$(head -2 <<< "$out" | tail -1 | cut -d":" -f1)
-    
-    # if ctrl-e: edit, else: 
+
+    # if ctrl-e: edit, else:
     if [ -n "$file" ]; then
         [ "$key" = ctrl-e ] && $EDITOR "$file" || tail -n +$((N+1)) "$file" | clipcopy
     fi
@@ -49,4 +49,4 @@ clipcopy() {
     fi
 }
 
-sl "$1"
+sl "$@"
